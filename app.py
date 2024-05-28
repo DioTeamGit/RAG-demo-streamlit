b5c895b484d0a5d44f092c3a94cf48e9323b9fbf
DioTeamGit <109144447+DioTeamGit@users.noreply.github.com>
2024-05-28 16:50:27 +0200

    updated temperature e altro

--- app.py
+++ app.py
@@ -11,14 +11,19 @@ from llama_index.embeddings.openai import OpenAIEmbedding
 from llama_index.vector_stores.qdrant import QdrantVectorStore
 from llama_index.core import Settings
 
+
+
+
 st.set_page_config(page_title="Q&A con documenti", page_icon="⚖️", layout="centered", initial_sidebar_state="auto", menu_items=None)
 openai.api_key = st.secrets.openai_key_p
 st.title("Q&A con documenti")
+
          
 st.sidebar.title("Seleziona i parametri di input")
-    
-    # Temperature slider for response creativity
 temperature = st.sidebar.slider("Seleziona la creatività della risposta", min_value=0.0, max_value=1.0, value=0.5, step=0.01)
+
+Settings.embed_model = OpenAIEmbedding(model="gpt-4o", temperature=temperature)
+Settings.llm = OpenAIEmbedding(model="text-embedding-3-small", embed_batch_size=100)  
 
     # Response format options
 format = st.sidebar.radio("Seleziona formato", options=['E-mail', 'Paragrafo', 'Lista', 'Formato Libero'])
@@ -48,8 +53,8 @@ if "messages" not in st.session_state.keys(): # Initialize the chat messages his
         {"role": "assistant", "content": "Inizia una chat con i tuoi documenti!"}
     ]
 client = qdrant_client.QdrantClient('https://46e915dc-c126-4445-af6d-265c738b7848.us-east4-0.gcp.cloud.qdrant.io:6333', api_key=st.secrets["qdrant_key"])
-vector_store = QdrantVectorStore(client=client, collection_name="RAG_2")
-index = VectorStoreIndex.from_vector_store(vector_store=vector_store)
+vector_store = QdrantVectorStore(client=client, collection_name="RAG_3")
+index = VectorStoreIndex.from_vector_store(vector_store=vector_store, )
 
 
 if "chat_engine" not in st.session_state.keys(): # Initialize the chat engine
@@ -62,8 +67,6 @@ if prompt := st.chat_input("Fai una domanda"): # Prompt for user input and save 
 for message in st.session_state.messages: # Display the prior chat messages
     with st.chat_message(message["role"]):
         st.write(message["content"])
-
-
 
 
 if st.session_state.messages[-1]["role"] != "assistant":
