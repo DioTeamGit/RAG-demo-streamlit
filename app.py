e6e1b83b492f2c7ce810cbccf0e646ad420a9a31
DioTeamGit <109144447+DioTeamGit@users.noreply.github.com>
2024-05-28 18:29:24 +0200

    minor changes

--- app.py
+++ app.py
@@ -56,18 +56,19 @@ client = qdrant_client.QdrantClient('https://46e915dc-c126-4445-af6d-265c738b784
 vector_store_4 = QdrantVectorStore(client=client, collection_name="RAG_4")
 index = VectorStoreIndex.from_vector_store(vector_store=vector_store_4)
 
+print(index)
 
 if "chat_engine" not in st.session_state.keys(): # Initialize the chat engine
         st.session_state.chat_engine = index.as_chat_engine(chat_mode="openai", verbose=True)
 
 print(st.session_state.selected_query)
 if prompt := st.chat_input("Fai una domanda"): # Prompt for user input and save to chat history
-    st.session_state.messages.append({"role": "user", "content": prompt + "Nel seguente formato:" + query_texts[st.session_state.selected_query]})
+    st.session_state.messages.append({"role": "user", "content": "Alway use the document in the index to answer:" + prompt})
 
 
 for message in st.session_state.messages: # Display the prior chat messages
     with st.chat_message(message["role"]):
-        st.write(message["content"])
+        st.write(prompt)
 
 
 if st.session_state.messages[-1]["role"] != "assistant":
