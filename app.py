6183736c43e6425a9490069ccfe92c2ac2d9fa0e
DioTeamGit <109144447+DioTeamGit@users.noreply.github.com>
2024-05-30 11:44:12 +0200

    prova prompt = none

--- app.py
+++ app.py
@@ -98,6 +98,7 @@ if st.session_state.selected_query != None:
 if prompt: # Prompt for user input and save to chat history
     st.session_state.messages.append({"role": "user", "content":  prompt})
     st.session_state_selected_query=None
+    prompt = None
 
 for message in st.session_state.messages: # Display the prior chat messages
     with st.chat_message(message["role"]):
