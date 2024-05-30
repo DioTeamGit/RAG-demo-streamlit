4eafd79be52a2979540ddfe25666c85b73433294
DioTeamGit <109144447+DioTeamGit@users.noreply.github.com>
2024-05-30 12:14:13 +0200

    change layout prova

--- app.py
+++ app.py
@@ -56,41 +56,43 @@ if 'selected_query' not in st.session_state:
     st.session_state.selected_query = None
 
 # Legal query buttons in Italian
-st.write("Scegli un prompt")
-query_texts = {
-    "Termini del Contratto": "Spiega le considerazioni chiave per il seguente contratto nazionale",
-    "Diritti dei Dipendenti": "Descrivi i diritti dei dipendenti riguardo al pagamento degli straordinari.",
-    "Norme sul Licenziamento": "Quali sono le basi legali per il licenziamento?",
-    "Sicurezza sul Lavoro": "Riassumi le responsabilità del datore di lavoro per la sicurezza sul lavoro."
-}
-
-for key, value in query_texts.items():
-    if st.button(value):
-        st.session_state.selected_query = value
-
-# Display the response in the main area if a query is selected
-
-if "messages" not in st.session_state.keys(): # Initialize the chat messages history
-    st.session_state.messages = [
-        {"role": "assistant", "content": "Inizia una chat con i tuoi documenti!"}
-    ]
-client = qdrant_client.QdrantClient('https://46e915dc-c126-4445-af6d-265c738b7848.us-east4-0.gcp.cloud.qdrant.io:6333', api_key=st.secrets["qdrant_key"])
-vector_store_4 = QdrantVectorStore(client=client, collection_name=selection)
-index = VectorStoreIndex.from_vector_store(vector_store=vector_store_4)
-
-print(index)
-
-
-
-if "chat_engine" not in st.session_state.keys(): # Initialize the chat engine
-        st.session_state.chat_engine = index.as_chat_engine(chat_mode="openai", verbose=True)
-
-print(st.session_state.selected_query)
 
 # qui cerco di 
 col1, col2 = st.columns([5,1])
 
 with col1:
+    st.write("Scegli un prompt")
+    query_texts = {
+        "Termini del Contratto": "Spiega le considerazioni chiave per il seguente contratto nazionale",
+        "Diritti dei Dipendenti": "Descrivi i diritti dei dipendenti riguardo al pagamento degli straordinari.",
+        "Norme sul Licenziamento": "Quali sono le basi legali per il licenziamento?",
+        "Sicurezza sul Lavoro": "Riassumi le responsabilità del datore di lavoro per la sicurezza sul lavoro."
+    }
+
+    for key, value in query_texts.items():
+        if st.button(value):
+            st.session_state.selected_query = value
+
+    # Display the response in the main area if a query is selected
+
+    if "messages" not in st.session_state.keys(): # Initialize the chat messages history
+        st.session_state.messages = [
+            {"role": "assistant", "content": "Inizia una chat con i tuoi documenti!"}
+        ]
+    client = qdrant_client.QdrantClient('https://46e915dc-c126-4445-af6d-265c738b7848.us-east4-0.gcp.cloud.qdrant.io:6333', api_key=st.secrets["qdrant_key"])
+    vector_store_4 = QdrantVectorStore(client=client, collection_name=selection)
+    index = VectorStoreIndex.from_vector_store(vector_store=vector_store_4)
+
+    print(index)
+
+
+
+    if "chat_engine" not in st.session_state.keys(): # Initialize the chat engine
+            st.session_state.chat_engine = index.as_chat_engine(chat_mode="openai", verbose=True)
+
+    print(st.session_state.selected_query)
+
+
     prompt=st.chat_input("Fai una domanda")
     #se seleziono il prompt dai buttons lo sovracrivo
     if st.session_state.selected_query != None:
