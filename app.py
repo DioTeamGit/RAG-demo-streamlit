f919b2f2930c2199964749831b40875e6e6b899f
DioTeamGit <109144447+DioTeamGit@users.noreply.github.com>
2024-06-14 10:22:42 +0200

    tolto chat engine

--- app.py
+++ app.py
@@ -209,7 +209,7 @@ with col2:
     def reset_conversation():
         # Reset chat history and any other relevant state variables
         st.session_state.chat_history = []
-        st.session_state.chat_engine.chat_history.clear()
+        #st.session_state.chat_messag.chat_history.clear()
         # Clear the screen by rerunning the app
         st.session_state.messages=[{"role": "assistant", "content": "Ciao, come posso esserti utile?"}]
 
