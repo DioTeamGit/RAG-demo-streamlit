fd3200223a859a3637f2f417ffbcf850791e0b32
DioTeamGit <109144447+DioTeamGit@users.noreply.github.com>
2024-06-14 10:52:26 +0200

    added user

--- app.py
+++ app.py
@@ -165,6 +165,8 @@ for message in st.session_state.messages:
 
 if prompt: # Prompt for user input and save to chat history
     st.session_state.messages.append({"role": "user", "content":  prompt})
+    with st.chat_message("user"):
+            st.markdown(prompt)
     client.beta.threads.messages.create(
     thread_id=st.session_state.thread_id,
     role="user",
