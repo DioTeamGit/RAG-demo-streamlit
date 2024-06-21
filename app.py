2a608f813473999500951286cdf985bccbd6ab1c
DioTeamGit <109144447+DioTeamGit@users.noreply.github.com>
2024-06-21 17:23:05 +0200

    Update app.py

--- app.py
+++ app.py
@@ -142,10 +142,10 @@ query_texts_dgai = {
 query_texts_dict = {"FISGR":query_texts_fisgr, "CCNL e Sentenze cassazione":query_texts_ccnl_cass, "AI ACT e Data Governance Act":query_texts_dgai}
 
 query_texts= query_texts_dict[selection]
-with streamlit_analytics.track():
-  for key, value in query_texts.items():
-      if st.button(key):
-          st.session_state.selected_query = value
+
+for key, value in query_texts.items():
+    if st.button(key):
+        st.session_state.selected_query = value
 
 # Display the response in the main area if a query is selected
 
@@ -166,7 +166,7 @@ if "chat_engine" not in st.session_state.keys(): # Initialize the chat engine
 
 #st.write(st.session_state.chat_engine)
 
-with streamlit_analytics.track():
+with streamlit_analytics.track(verbose=True):
   prompt=st.chat_input("Fai una domanda")
   #se seleziono il prompt dai buttons lo sovracrivo
   if st.session_state.selected_query != None:
